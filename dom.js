c05a959e59b5fe183ea000b16f0382419a25fb7d
Rakeshhub <fl2rakesh77@gmail.com>
2023-04-07 18:07:25 +0530

    edited

---
+++ dom.js
@@ -0,0 +1,21 @@
+
+
+
+
+// var parentnode= document.getElementById("items");
+// //var hello= <li>hello</>
+
+// //parentnode.appendFirstChild(hello);
+
+// parentnode.innerHTML="<li> <b>HELLO</li>" + parentnode.innerHTML
+
+
+// var items=document.getElementById("header-title");
+
+// items.innerHTML="<li> <b>HELLO</li>" + items.innerHTML
+
+
+
+
+
+
